2d266256b1d49f0b5d705434d54eb7285ce2ae3d
KeltarHelviett <keltarhelviett@gmail.com>
2017-10-16 01:17:03 +1000

    Added parser testing

--- test.sh
+++ test.sh
@@ -12,26 +12,41 @@ then
 else
 	echo "tcc was found"
 fi
-i=0
-j=0
-for file in $(ls tests/scanner/ --ignore=*.out | sort -n)
+ti=0
+tj=0
+for file in $(ls tests/tokenizer/ --ignore=*.out | sort -n)
 do
-	j=$(($j + 1))
+	tj=$(($tj + 1))
 	prefix=$( echo "$file" | sed -e "s/\.cpp//g" )
-	eval "./$tccpath -t tests/scanner/$file > tests/scanner/temp.txt"
-	cmp -s "tests/scanner/$prefix.out" "tests/scanner/temp.txt"
+	eval "./$tccpath -t tests/tokenizer/$file > tests/tokenizer/temp.txt"
+	cmp -s "tests/tokenizer/$prefix.out" "tests/tokenizer/temp.txt"
 	if [ $? -eq 1 ]; then
 		echo "$file test failed"
 	else
 		echo "$file test succed"
-		i=$(($i + 1))
+		ti=$(($ti + 1))
 	fi
 done
-rm tests/scanner/temp.txt
-echo "passed $i/$j"
-
-
-
+rm tests/tokenizer/temp.txt
+echo "Tokenizer: passed $ti/$tj"
+pi=0
+pj=0
+for file in $(ls tests/parser/ --ignore=*.out | sort -n)
+do
+	pj=$(($pj + 1))
+	prefix=$( echo "$file" | sed -e "s/\.cpp//g" )
+	eval "./$tccpath -t tests/parser/$file > tests/parser/temp.txt"
+	cmp -s "tests/parser/$prefix.out" "tests/parser/temp.txt"
+	if [ $? -eq 1 ]; then
+		echo "$file test failed"
+	else
+		echo "$file test succed"
+		pi=$(($pi + 1))
+	fi
+done
+rm tests/parser/temp.txt
+echo "Parser: passed $pi/$pj"
+echo "Total:  passed $(($pi+$ti))/$(($tj+$pj))"
 
 
 
