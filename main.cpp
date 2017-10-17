c1cef9ce12c5a1c8a972e6a54dc09692ef88b658
KeltarHelviett <keltarhelviett@gmail.com>
2017-10-17 21:44:14 +1000

    Changed test appearance a bit

--- main.cpp
+++ main.cpp
@@ -3,43 +3,34 @@
 #include <parser.h>
 #include "includes/args.hxx"
 
-args::ArgumentParser parser("This is tcc - Tiny C Compiler", "");
-args::HelpFlag help(parser, "help", "Display this menu", {'h', "help"});
-args::Flag parseOnly(parser, "parse-only", "Parse only", {'p', "parse-only"});
-args::Flag tokenizeOnly(parser, "tokenize-only", "Tokenize only", {'t', "tokenize-only"});
-args::Positional<std::string> sourceFile(parser, "source file(s)", "The name(s) of source file(s)");
+args::ArgumentParser argParser("This is tcc - Tiny C Compiler", "");
+args::HelpFlag help(argParser, "help", "Display this menu", {'h', "help"});
+args::Flag parseOnly(argParser, "parse-only", "Parse only", {'p', "parse-only"});
+args::Flag tokenizeOnly(argParser, "tokenize-only", "Tokenize only", {'t', "tokenize-only"});
+args::Positional<std::string> sourceFile(argParser, "source file(s)", "The name(s) of source file(s)");
 
 void TokenizeOnly()
 {
     Tokenizer tok;
     printf("Position\t\tType\t\t\tValue\t\t\tText\n\n");
-    try
-    {
-        tok.OpenFile(sourceFile.Get());
-        Token *t;
+    tok.OpenFile(sourceFile.Get());
+    Token *t;
+    while ((t = tok.Next())->type != TokenType::END_OF_FILE)
+        switch (t->type)
+        {
+            case TokenType::NUM_INT:
+                printf("(%d, %d)\t\t\t%s\t\t\t%llu\t\t\t\t  %s\n", t->row, t->col,
+                       TokenTypeToString[t->type].c_str(), t->intValue,t->text.c_str());
+                break;
+            case TokenType::NUM_FLOAT:
+                printf("(%d, %d)\t\t\t%s\t\t\t%llf\t\t\t\t  %s\n", t->row,
+                       t->col, TokenTypeToString[t->type].c_str(), t->floatValue, t->text.c_str());
+                break;
+            default:
+                printf("(%d, %d)\t\t\t%s\t\t\t%s\t\t\t\t  %s\n", t->row, t->col,
+                       TokenTypeToString[t->type].c_str(), t->stringValue,t->text.c_str());
+        }
 
-        while ((t = tok.Next())->type != TokenType::END_OF_FILE)
-            switch (t->type)
-            {
-                case TokenType::NUM_INT:
-                    printf("(%d, %d)\t\t\t%s\t\t%llu\t\t%s\n", t->row, t->col,
-                           TokenTypeToString[t->type].c_str(), t->intValue,t->text.c_str());
-                    break;
-                case TokenType::NUM_FLOAT:
-                    printf("(%d, %d)\t\t\t%s\t\t%llf\t\t%s\n", t->row,
-                           t->col, TokenTypeToString[t->type].c_str(), t->floatValue, t->text.c_str());
-                    break;
-                default:
-                    printf("(%d, %d)\t\t\t%s\t\t%s\t\t%s\n", t->row, t->col,
-                           TokenTypeToString[t->type].c_str(), t->stringValue,t->text.c_str());
-            }
-
-    }
-    catch (LexicalError e)
-    {
-        std::cout << e.what();
-        exit(1);
-    }
 }
 
 void ParseOnly()
@@ -54,34 +45,42 @@ int main(int argc, char **argv)
 
     try
     {
-        parser.ParseCLI(argc, argv);
+        argParser.ParseCLI(argc, argv);
     }
     catch (args::Help)
     {
-        std::cout << parser;
+        std::cout << argParser;
         return 0;
     }
     catch (args::ParseError &e)
     {
         std::cerr << e.what() << std::endl;
-        std::cerr << parser;
+        std::cerr << argParser;
         return 1;
     }
     catch (args::ValidationError &e)
     {
         std::cerr << e.what() << std::endl;
-        std::cerr << parser;
+        std::cerr << argParser;
         return 1;
     }
     if (!sourceFile)
         throw new std::exception();
-    if (tokenizeOnly)
+    try
     {
-        TokenizeOnly();
+        if (tokenizeOnly)
+        {
+            TokenizeOnly();
+        }
+        else if (parseOnly)
+        {
+            ParseOnly();
+        }
     }
-    else if (parseOnly)
+    catch (CompilationError &e)
     {
-        ParseOnly();
+        std::cout << e.what() << std::endl;
+        return 1;
     }
 	return 0;
 }
